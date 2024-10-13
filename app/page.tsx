68ffd9a7a9f9b15c0e42b790a77088285fb18e9c
AlphaBetaGama011 <91762451+AlphaBetaGama011@users.noreply.github.com>
2024-10-12 20:58:24 -0700

    Tried to render to mobile screen

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ import { MdEmail } from "react-icons/md";
 import { useState, useEffect } from "react";
 import { Link, Element } from 'react-scroll';
 import Footer from "../components/Footer";
+import MobileFooter from "../components/MobileFooter";
 import { InstagramEmbed } from 'react-social-media-embed';
 import { Homemade_Apple } from "next/font/google";
 import { useInView } from "react-intersection-observer";
@@ -31,9 +32,9 @@ export default function Page() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // This matches Tailwind's 'md' breakpoint
+      setIsMobile(window.innerWidth < 1024); // This matches Tailwind's 'md' breakpoint
     };
-
+    handleResize();
     const handleScroll = () => {
       const elements = document.querySelectorAll('.hidden');
       elements.forEach((element) => {
@@ -57,133 +58,78 @@ export default function Page() {
 
 
   if (isMobile) {
-    return (
-      <div className='pb-9'>
-        <MobileHeader />
-        {/* <Navbar /> */}
-        <div className='mt-[-20%] poinpowert-scroll-container'>
-          {/* <div className='min-h-screen bg-gray-100'> */}
-          {/* <main> */}
-
-          <section
-            id='home'
-            className='mb-2 min-h-screen max-w-screen flex flex-col items-center justify-center text-white px-4 py-8 custom-book pb-[20%]'>
-            {/* <div className='items-center text-center'> */}
-            <img
-              className='max-w-[70%] max-h-[70%] book-img-blur book-img-style'
-              src='/HinduYUVA@UCRF2024.jpg'
-            />
-            <h2 className='text-center tah-clear-sans pt-[15%]'>
-              Hindu YUVA aims to provide a platform to preserve, practice, promote, and protect
-              Hindu Dharma by bringing together Hindu youth on college campuses across North America.
-              Hindu YUVA creates opportunities for college communities (students, staff, and faculty members)
-              to understand and practice Hindu Dharma or the Hindu way of life.
-            </h2>
-
-            {/* <Button className='buy-book-button border-1px-black mt-8' onClick={buyBook}>
-              Buy Now
-            </Button> */}
-            {/* </div> */}
-          </section>
-          <div className='bg-gradient-to-br from-red-600 via-navy-900 to-sky-400'>
-            <section
-              id='about'
-              className='min-h-screen flex flex-col items-center justify-center text-white px-4 py-8'>
-              <div className='w-full lg:w-1/2 flex justify-center mt-[-5%]'>
-                <div className='powerpoint-scroll-container-about-us relative w-40 h-40 xs:w-72 xs:h-72 sm:w-80 sm:h-80 lg:w-[450px] lg:h-[450px] pt-[-70%] mt-[-5%] mb-[-16%] '>
-                  {" "}
-                  {/* Decreased image size */}
-
+      return (
+        <>
+          <div className='pb-9'>
+          <MobileHeader />
+            
+            
+              <section id="#home">
+                <div className='slider gradient  mt-[-42.5%] '>
+                  <figure>
+                    <img src="/1.png" alt="image1" />
+                    <img src="/2.png" alt="image1" />
+                    <img src="/3.png" alt="image1" />
+                  </figure>
                 </div>
-              </div>
-              <div className='mx-auto pt-[30%]'>
-                <p className='text-l font-semibold text-center mb-4'>
-                  The American Hindu is a dynamic, youth-driven initiative that
-                  began as an Instagram content creator in January 2020.
-                </p>
-                <p className='font-light text-s text-center mb-1 '>
-                  The American Hindu has evolved into a self-publishing
-                  organization specializing in a diverse range of Hindu
-                  educational content. Through a blend of traditional and modern
-                  mixed-media formats, including print and digital publications,
-                  videos, and interactive resources, the organization aims to
-                  make Hindu culture, philosophy, and history accessible and
-                  engaging for a wide audience. By empowering young creators and
-                  fostering a collaborative environment, The American Hindu
-                  preserves and promotes cultural heritage and encourages the
-                  exploration and understanding of Hindu traditions in a
-                  contemporary context.
-                </p>
-              </div>
-            </section>
-          </div>
-
-          <section
-            id='join'
-            className='mt-[-10%] min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400'>
-            <div className='container mx-auto px-4 max-w-7xl'>
-              <div className='flex flex-col md:flex-row items-start justify-between'>
-                <div className='text-white md:w-2/3 pr-0 md:pr-8'>
-                  <h2 className='mt-[-20%] text-4xl xs:text-5xl lg:text-6xl font-bold mb-4 text-left'>
-                    Join Our Community
-                  </h2>
-                  <p className='text-xl sm:text-2xl mb-6 text-left'>
-                    {/* No mailing list just social media and email */}
-                    Follow us on social media to stay updated on our mission to
-                    create Hindu representation in mixed media.
-                  </p>
-                  <div className='flex flex-row gap-[2%]'>
-                    {/* <a
-                    href='https://forms.gle/nD3DvXgjB5pTeuwy8'
+               
+                <div className='banner-text'>
+                  <h1 className=''> Hindu YUVA at </h1>
+                  <h1>The University of California, Riverside</h1>
+                  <h2 className='text-2xl mt-7'>A platform to preserve, practice, promote, and protect Dharma at UCR.</h2>
+                  <a
                     target='_blank'
-                    rel='noopener noreferrer'
-                    className='inline-block'>
-                    <button className='bg-white text-black font-bold py-2 px-6 rounded-full hover:bg-gray-200 hover:scale-105 transition duration-300'>
+                    className=''
+                    href='https://highlanderlink.ucr.edu/organization/hinduyuvaucr'>
+                    <button className=' mt-[8%] w-60 buy-book-button-before text-white hover:text-black font-bold py-3 sm:py-4 px-9 sm:px-12 rounded-full hover:bg-blue-100 transform hover:scale-105 transition duration-300 ease-in-out shadow-lg text-xl sm:text-2xl '>
                       Join Now
                     </button>
-                  </a> */}
-                    {/* <div className='border-2 border-color-black pr-[11%] pt-[1%]'> */}
-                    <div className='text-3xl flex flex-row align-middle gap-[15%] pt-[1.5%] '>
-                      <a
-                        target='_blank'
-                        href='https://facebook.com/TheAmericanHinduPage'>
-                        <BsFacebook />
-                      </a>
-                      <a
-                        target='_blank'
-                        href='https://instagram.com/theamericanhindu'>
-                        <FaInstagram />
-                      </a>
-                      <a target='_blank' href='https://x.com/American_Hindu'>
-                        <BsTwitterX />
-                      </a>
-                      <a
-                        target='_blank'
-                        href='mailto:theamericanhindu@gmail.com'>
-                        <MdEmail />
-                      </a>
-                      {/* </div> */}
+                  </a>
+                </div>
+              </section>
+
+              <div className='about'>
+                <section
+                  id='about'
+                  className='min-h-screen flex flex-col items-center justify-center text-white px-4 py-8'>
+                  <div className='w-full lg:w-1/2 flex justify-center mt-[-5%]'>
+                    <div className='relative w-40 h-40 xs:w-72 xs:h-72 sm:w-80 sm:h-80 lg:w-[450px] lg:h-[450px] pt-[-70%] mt-[-5%] mb-[-16%] '>
+                      {" "}
+                      {/* Decreased image size */}
                     </div>
                   </div>
-                </div>
+                  <h2 className='text-3xl font-bold justify-center mt-[-10%]'>Who Are We?</h2>
+                  <img
+                    className='max-w-[100%] max-h-[55%] book-img-blur book-img-style'
+                    src='/HinduYUVAPerson.png'
+                  />
+                  <div className='mx-auto pt-[30%]'>
+                    <p className='text-xl font-semibold text-center mb-10'>
+                      Hindu YUVA aims to provide a platform to preserve, practice, promote, and protect
+                      Hindu Dharma by bringing together Hindu youth on college campuses across North America.
+                      Hindu YUVA creates opportunities for college communities (students, staff, and faculty members)
+                      to understand and practice Hindu Dharma or the Hindu way of life.
+                    </p>
+                  </div>
+                </section>
               </div>
+              <section id="#youtube" ref={youtubeRef} className={`transition-opacity duration-1000 ${youtubeInView ? 'opacity-100' : 'opacity-0'}`}>
+                <section className='drop-shadow-2xl youtube-background mt-[-7%] pb-2 '>
+                  <div className=''>
+                    <iframe width="370" height="250" src="https://www.youtube-nocookie.com/embed/SOLQo1HFOTY?si=wTMGNQBZla7NO8xx&amp;start=0"
+                      title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"></iframe>
+                  </div>
+                </section>
+              </section>
+
+              <Element name='footer'>
+                <MobileFooter />
+              </Element>
             </div>
-          </section>
-          <section
-            id="instagram-feed"
-            className='min-h-screen flex flex-col items-center justify-center text-white px-4 py-8 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400'>
-            <div className='mt-16 mb-(-4)'
-              style={{ display: 'flex', justifyContent: 'center' }}>
-              <InstagramEmbed url="https://www.instagram.com/p/DA1lznXy6BG/?igsh=MzRlODBiNWFlZA==" width={328} captioned />
-            </div>
-            <p className='fixed bottom-1 opacity-50 left-2'>
-              © 2024 The American Hindu. All rights reserved.
-            </p>
-          </section>
-        </div>
-      </div>
-    );
-  } else {
+        </>
+      );
+    } else {
     return (
       <>
         <Header />
--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,33 +1,114 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import { BsFacebook, BsInstagram, BsTwitter, BsTwitterX, BsYoutube } from 'react-icons/bs'
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsTwitterX,
+  BsYoutube,
+} from "react-icons/bs";
 import "../styles/Header.css";
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
-
+import { usePathname } from "next/navigation";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { MdClose, MdMenu } from "react-icons/md";
+import Link from "next/link";
 
 function MobileHeader() {
+  const [menu, setMenu] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const pathname = usePathname();
+
+  const menuLeave = () => {
+    const coolMenu = document.getElementById("cool-menu");
+    if (coolMenu !== null) {
+      coolMenu.classList.add("menu-leave");
+      coolMenu.classList.remove("menu-enter");
+    }
+
+    setTimeout(() => {
+      setMenu(false);
+    }, 400);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ["home", "about", "join"];
+      const currentSection = sections.find((section) => {
+        const element = document.getElementById(section);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+
+          return rect.top <= 100 && rect.bottom >= 100;
+        }
+        return false;
+      });
+      setActiveSection(currentSection || "");
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    setActiveSection(pathname.slice(1) || "home");
+  }, [pathname]);
+
+  const navItems = [
+    { name: "Home", href: "#home" },
+    { name: "About Us", href: "#about" },
+    { name: "Contact", href: "#join" },
+    {name: "Instagram", href: '#instagram-feed'}
+  ];
+
   return (
     <>
+      <div className='pl-2 d-header flex-col items-center '>
+        <div>
+          <img
+            src='HinduYUVA-NBUCR.png'
+            alt='The American Hindu Logo'
+            className='mt-1 ml-1 w-60 h-60 object-cover rounded-full overflow-hidden max-w-[60px] max-h-[60px] bg-red-100'
+          />
+          <MdMenu
+            className='open-menu position-absolute top-0 mt-2'
+            size={70}
+            onClick={() => {
+              setMenu(true);
+            }}
+          />
 
-    <div className='mx-auto flex justify-center flex-col items-center mb-4 bg-gradient-to-br from-red-600 via-navy-900 to-sky-400'>
-                <img
-                    src="swami.jpg"
-                    alt="The American Hindu Logo"
-                    className="w-50 h-50 object-cover rounded-full overflow-hidden max-w-[100px] max-h-[100px] bg-red-100 mt-2"
-                />
-            
-    
-    <div className="header-socials">
+          {menu && (
+            <div className='menu-container menu-enter' id='cool-menu'>
+              <MdClose className='close-menu' onClick={menuLeave} />
+              {/* <p className='menu-btn'>Home</p> */}
+              {navItems.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={`menu-btn pb-10${
+                    activeSection === item.href.slice(1)
+                      ? "menu-btn pb-10 pt-5"
+                      : ""
+                  }`}>
+                  <span className='relative inline-block menu-btn'>
+                    {item.name}
+                  </span>
+                </Link>
+              ))}
+            </div>
+          )}
+          {/* <p className='mb-4 ml-6 justify-center font-semibold	mobile-header-the-american'>The American</p>
+          <p className =' ml-10 justify-center font-bold		mobile-header-hindu'>Hindu</p> */}
+        </div>
+
+        {/* <div className="header-socials">
     <a target="_blank" href="https://google.com"><BsFacebook/></a>
     <a target="_blank" href="https://google.com"><BsInstagram/></a>
     <a target="_blank" href="https://google.com"><BsTwitterX/></a>
     <a target="_blank" href="https://google.com"><BsYoutube/></a>
-    </div>
-    
-    </div>
-  </>
-  )
+    </div> */}
+      </div>
+    </>
+  );
 }
 
 export default MobileHeader;
-
